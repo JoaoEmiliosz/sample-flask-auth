9f47188805d9d56e0470162ea1d8009f8f955617
joao emilio <joaoemilio785@gmail.com>
2024-05-02 08:09:57 -0300

    Implementing user roler

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 from database import db
 from flask_login import UserMixin
+
 class User(db.Model, UserMixin):
     #id (int), username (text), passwword (text)
     id = db.Column(db.Integer, primary_key=True)
